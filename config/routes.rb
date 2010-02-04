71ff662c94e9feaefbc4502aa6b741a22cd41225
Your Name <you@example.com>
2010-02-04 21:32:40 +0100

    q

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
 
-  map.connect '', :controller => 'faysbook'
+  map.connect '', :controller => 'feysbook'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
