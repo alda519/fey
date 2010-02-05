038d3201a0feabaa673e967e69ee21b852d6038a
Your Name <you@example.com>
2010-02-05 22:03:39 +0100

    fix

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,18 @@
 class UserController < ApplicationController
 
+  before_filter :login_required, :only => [:show]
+
+  def index
+    redirect_to :controller => 'feysbook'
+  end
+
   def login
     if request.post?
       @current_user = User.find_by_login_and_password params[:login], Digest::SHA1.hexdigest(params[:password])
       unless @current_user.nil?
         session[:user_id] = @current_user.id
         # dopsat presmerovani na to, co jsem chtel delat
-        redirect_to :controller => 'feysbook'
+        redirect_to :action => 'show'
         flash[:notice] = 'logged in'
       end
       # flash[:notice] = 'POST'
@@ -25,6 +31,7 @@ class UserController < ApplicationController
   def register
     @user = User.new params[:register]
     if request.post? and @user.save
+      session[:user_id] = @current_user.id
       redirect_to :controller => 'user', :action => 'show'
     end
   end
