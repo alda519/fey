16dee5a5613e387e2a3e0299dcc37da4e60152b3
Your Name <you@example.com>
2010-02-05 18:52:11 +0100

    register login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,25 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  before_filter :fetch_logged_in_user
+
+  protected
+
+  def fetch_logged_in_user
+    return if session[:user_id].blank?
+    @current_user = User.find_by_id session[:user_id]
+  end
+
+  def logged_in?
+    ! @current_user.blank?
+  end
+  helper_method :logged_in?
+
+  def login_required
+    return true if logged_in?
+    session[:return_to] = request.requested_uri
+    redirect_to :controller => 'user', :action => 'login' and return false
+  end
+
 end
--- app/controllers/feysbook_controller.rb
+++ app/controllers/feysbook_controller.rb
@@ -1,6 +1,9 @@
 class FeysbookController < ApplicationController
 
   def index
+    unless logged_in?
+      redirect_to :controller => 'user', :action => 'register'
+    end
   end
 
 end
