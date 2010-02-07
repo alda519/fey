8db2ba03c89279f58b897b0812b0cc63d6b73b7b
Alda <you@example.com>
2010-02-07 13:21:40 +0100

    new contrl

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,7 @@ class UserController < ApplicationController
   before_filter :login_required, :only => [:show, :logout, :wall, :foto]
 
   def index
-    redirect_to :controller => 'feysbook'
+    redirect_to :action => 'show'
   end
 
   def login
@@ -25,31 +25,25 @@ class UserController < ApplicationController
     session[:user_id] = @current_user = nil
   end
 
-  def show
-    @latest_brick = @current_user.bricks.find :last
-  end
-
   def register
-    @user = User.new params[:register]
+    @user = User.new params[:user]
     if request.post? and @user.save
       session[:user_id] = @user.id
-      redirect_to :controller => 'user', :action => 'show'
+      redirect_to :action => 'show'
     end
+  end
+
+
+  def show
+    @latest_brick = @current_user.bricks.find :last
+    session[:page] = 'user'
   end
 
   def wall
-    @latest_bricks = @current_user.bricks.find :all, :order => 'created_at desc', :limit => 8
+    redirect_to :controller => 'wall'
   end
 
   def foto
-  end
-
-  def newbrick
-    @brick = Brick.new params[:brick]
-    @brick.user_id = @current_user.id
-    if request.post? and @brick.save
-      redirect_to :action => 'wall'
-    end
   end
 
 end
