af002be8c1d0ec0c4313b48ec881b5de39a0316b
Alda <you@example.com>
2010-02-07 20:13:28 +0100

    1st security update

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -1,8 +1,11 @@
 class WallController < ApplicationController
 
+  before_filter :login_required
+
   def index
     @latest_bricks = @current_user.bricks.find :all, :order => 'created_at desc', :limit => 8
     session[:page] = 'wall'
+    @enemies = User.find :all, :order => 'rand()', :conditions => "id != #{@current_user.id}", :limit => 8
   end
 
   def newbrick
@@ -15,8 +18,12 @@ class WallController < ApplicationController
 
   def delete
     @brick = Brick.find_by_id params[:id]
-    @brick.delete
-    redirect_to :controller => session[:page]
+    if @brick.user_id == @current_user.id
+      @brick.delete
+    else
+      flash[:warning] = 'Tento nejde smazat'
+    end
+      redirect_to :controller => session[:page]
   end
 
   def edit
