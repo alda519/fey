eb61ba789ad3bb0233a88ee3ac21e69c9e7bea73
Alda <you@example.com>
2010-02-19 17:44:36 +0100

    BBcode alfa

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,27 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def bbCode data
+    #newline
+    data.gsub! "\n", "<br />"
+
+    # bold, italics, underline, code [x] [/x]
+    data.gsub! /\[(\/|)(b|u|i|code)\]/, '<\1\2>'   # b u i
+
+    #text color TODO
+    # [color=kod][/color]
+
+    #text size TODO
+    # [size=procenta][/size]
+
+    #img [img]source[/img]
+    x = data.split '[img]'
+    x.each_index do |i|
+      x[i].gsub! /(.*)\[\/img\]/, '<img src="\1" /><br clear="both" />' 
+    end
+    data = x.join
+
+    data
+  end
+
 end
