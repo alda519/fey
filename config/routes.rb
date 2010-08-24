72f2be7243c8a8e0f3a0487dbdd3841777237ae9
alda519 <galda519@gmail.com>
2010-08-24 14:06:28 +0200

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
 
-  map.connect '', :controller => 'facebook'
+  map.connect '', :controller => 'faceboo'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
